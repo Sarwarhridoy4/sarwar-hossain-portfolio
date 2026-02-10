25aa8fc68d9dcaa9ef8c790dff9b3e87c3101156
Sarwar Hossain <sarwarhridoy4@gmail.com>
2026-02-10 17:23:58 +0600

    Adjust env config defaults

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,19 +5,18 @@ dotenv.config({ path: ".env" });
 
 export const env = {
   PORT: process.env.PORT || 5000,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL || "",
   BYCRYPT_SALT_ROUNDS: process.env.BYCRYPT_SALT_ROUNDS || 10,
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET_KEY: process.env.JWT_ACCESS_SECRET || "your_jwt_secret_key",
   JWT_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES || "1d",
   JWT_REFRESH_SECRET:
     process.env.JWT_REFRESH_SECRET || "your_jwt_refresh_secret",
-  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "your_nextauth_secret",
   JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES || "30d",
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "your_cloud_name",
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || "your_api_key",
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "your_api_secret",
   DATABASE_URL: process.env.DATABASE_URL || "your_database_url",
-  ADMIN_EMAIL: process.env.ADMIN_EMAIL || "",
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || "",
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
   FRONTEND_URL_PRODUCTION:
